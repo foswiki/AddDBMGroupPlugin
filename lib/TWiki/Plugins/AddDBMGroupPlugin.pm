02347a341c3419e203e0fb62064905ad6b1ee29c
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/AddDBMGroupPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/AddDBMGroupPlugin.pm
+++ lib/TWiki/Plugins/AddDBMGroupPlugin.pm
@@ -10,7 +10,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -26,67 +26,89 @@ package TWiki::Plugins::AddDBMGroupPlugin;
 use strict;
 use Fcntl;
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug @wikiGroups $groupFile
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+  $debug @wikiGroups $groupFile
+);
 
-  $VERSION = '$Rev$';
-  $pluginName = 'AddDBMGroupPlugin';
+$VERSION    = '$Rev$';
+$pluginName = 'AddDBMGroupPlugin';
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.021 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1.021 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
     # For some reason, getPluginPreferencesFlag doesn't work
-    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $topic, $web, $user, $installWeb ) started" ) if $debug;
+    $debug = TWiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
+    TWiki::Func::writeDebug(
+"- TWiki::Plugins::${pluginName}::initPlugin( $topic, $web, $user, $installWeb ) started"
+    ) if $debug;
 
-    @wikiGroups = split(/[[:space:],]+/, TWiki::Func::getPluginPreferencesValue( "WIKI_GROUP" ) || undef);
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin: wikiGroups is ( ".join(' | ', @wikiGroups)." )" ) if $debug;
+    @wikiGroups =
+      split( /[[:space:],]+/,
+        TWiki::Func::getPluginPreferencesValue("WIKI_GROUP") || undef );
+    TWiki::Func::writeDebug(
+            "- TWiki::Plugins::${pluginName}::initPlugin: wikiGroups is ( "
+          . join( ' | ', @wikiGroups )
+          . " )" )
+      if $debug;
 
-    $groupFile = TWiki::Func::getPluginPreferencesValue( "GROUP_FILE" ) || undef;
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin: groupFile is $groupFile" ) if $debug;
+    $groupFile = TWiki::Func::getPluginPreferencesValue("GROUP_FILE") || undef;
+    TWiki::Func::writeDebug(
+        "- TWiki::Plugins::${pluginName}::initPlugin: groupFile is $groupFile")
+      if $debug;
 
-    my $moduleList = TWiki::Func::getPluginPreferencesValue( "DBM_MODULES" ) || undef;
-    my @moduleList = (defined($moduleList) && $moduleList ne '') ? split(/[[:space:],]+/, $moduleList) : @TWiki::dbmmanageModules
-    ;
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin: moduleList is ( ".join(' | ', @moduleList)." )\n" ) if $debug;
+    my $moduleList = TWiki::Func::getPluginPreferencesValue("DBM_MODULES")
+      || undef;
+    my @moduleList =
+      ( defined($moduleList) && $moduleList ne '' )
+      ? split( /[[:space:],]+/, $moduleList )
+      : @TWiki::dbmmanageModules;
+    TWiki::Func::writeDebug(
+            "- TWiki::Plugins::${pluginName}::initPlugin: moduleList is ( "
+          . join( ' | ', @moduleList )
+          . " )\n" )
+      if $debug;
+
     # Load up the AnyDBM module
     eval {
-      @AnyDBM_File::ISA = @moduleList;
-      use AnyDBM_File ();
+        @AnyDBM_File::ISA = @moduleList;
+        use AnyDBM_File ();
     };
     if ($@) {
-        TWiki::Func::writeWarning( "Error using AnyDBM_File: $@" );
+        TWiki::Func::writeWarning("Error using AnyDBM_File: $@");
         return 1;
     }
 
     # Plugin correctly initialized
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    TWiki::Func::writeDebug(
+        "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $debug;
 
     return 1;
 }
 
 # =========================
-sub registrationHandler
-{
+sub registrationHandler {
     ### my ( $web, $wikiName, $loginName ) = @_;
     my %groups;
 
-    if (!defined($_[1]) || $_[1] eq '') {
-        TWiki::Func::writeWarning( "- ${pluginName}::registrationHandler: wikiName ($_[1]) is empty!" );
+    if ( !defined( $_[1] ) || $_[1] eq '' ) {
+        TWiki::Func::writeWarning(
+            "- ${pluginName}::registrationHandler: wikiName ($_[1]) is empty!");
         return;
     }
 
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::registrationHandler( $_[0], $_[1], $_[2] ) called" ) if $debug;
+    TWiki::Func::writeDebug(
+"- TWiki::Plugins::${pluginName}::registrationHandler( $_[0], $_[1], $_[2] ) called"
+    ) if $debug;
 
     # There's nothing to do if @wikiGroups is empty
     return unless @wikiGroups;
@@ -94,31 +116,37 @@ sub registrationHandler
     # There's nothing to do if $groupFile isn't specified
     return unless defined($groupFile);
 
-    if ($groupFile eq '') {
+    if ( $groupFile eq '' ) {
 
         # This shouldn't happen
-        TWiki::Func::writeWarning( "- TWiki::Plugins::${pluginName}::registrationHandler: empty WIKI_GROUP_FILE specified\n");
+        TWiki::Func::writeWarning(
+"- TWiki::Plugins::${pluginName}::registrationHandler: empty WIKI_GROUP_FILE specified\n"
+        );
         return;
 
-    } elsif (! -e $groupFile) {
+    }
+    elsif ( !-e $groupFile ) {
 
-        TWiki::Func::writeWarning( "- TWiki::Plugins::${pluginName}::registrationHandler: WIKI_GROUP_FILE doesn't exist!\n");
+        TWiki::Func::writeWarning(
+"- TWiki::Plugins::${pluginName}::registrationHandler: WIKI_GROUP_FILE doesn't exist!\n"
+        );
         return;
 
-    } else {
+    }
+    else {
 
-        if (tie %groups, 'AnyDBM_File', $groupFile, 0644, O_RDWR|O_CREAT) {
+        if ( tie %groups, 'AnyDBM_File', $groupFile, 0644, O_RDWR | O_CREAT ) {
 
             # Get a local copy of @wikiGroups, with modifications if necessary
             my %groupList = map { s/USER!/$_[1]/g; $_ => 1 } @wikiGroups;
 
-            if( exists($groups{$_[1]}) && $groups{$_[1]} ne '' ) {
+            if ( exists( $groups{ $_[1] } ) && $groups{ $_[1] } ne '' ) {
                 foreach my $group ( split( /,+/, $groups{$user} ) ) {
                     $groupList{$group} = 1;
                 }
             }
 
-            $groups{$_[1]} = join( ',', sort keys %groupList );
+            $groups{ $_[1] } = join( ',', sort keys %groupList );
             untie %groups;
         }
     }
